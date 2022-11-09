a6133cdd89b78b25ec219fe866fe64b76b7b58f2
Valentina Arias <ariasvalentinaail@gmail.com>
2022-11-08 21:35:25 -0300

    Se mejoró el template de ver y agregar salas.

--- ProyectoFinal/settings.py
+++ ProyectoFinal/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'ProyectoFinal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["E:/_CURSO_PYTHON_CH/vsc/ProyectoFinal_EntregaIntermedia/AppProyectoFinal/templates/"],
+        'DIRS': ["E:/_CURSO_PYTHON_CH/vsc/ProyectoFinal_EntregaIntermedia/AppProyectoFinal/static/css"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
